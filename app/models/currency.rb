33f7bfe03a5d97643ccbe2837f93d54a57747630
transfer76 <shina76@ukr.net>
2019-08-12 17:02:36 +0300

    Add culculate amaunt of currency to model

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,4 +1,8 @@
 class Currency < ApplicationRecord
+	def calculate_value(amount)
+		(current_price.to_f * amount.to_f).round(4)
+	end
+
 	def current_price
 		url = 'https://api.coinmarketcap.com/v1/ticker/'
 		request = HTTParty.get(url + self.slug)
