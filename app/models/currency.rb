e225e824e1a113ccbf5c6c3cc595b644a0950c9d
transfer76 <shina76@ukr.net>
2019-08-12 16:57:48 +0300

    Add getting current price of currency

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,2 +1,7 @@
 class Currency < ApplicationRecord
+	def current_price
+		url = 'https://api.coinmarketcap.com/v1/ticker/'
+		request = HTTParty.get(url + self.slug)
+		response = JSON.parse(request.body)[0]['price_usd']
+  end
 end
