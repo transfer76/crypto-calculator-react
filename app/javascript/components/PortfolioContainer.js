bbe5e7019b9b15573b44d9c2ba2be89ed211b4dd
transfer76 <shina76@ukr.net>
2019-08-13 15:01:37 +0300

    Delete debugger search

--- app/javascript/components/PortfolioContainer.js
+++ app/javascript/components/PortfolioContainer.js
@@ -8,7 +8,6 @@ class PortfolioContainer extends Component {
 	super(props)
 
 	this.state = {
-		name: '',
 		portfolio: [],
 		search_results: [],
 		active_currency: null,
@@ -31,20 +30,29 @@ class PortfolioContainer extends Component {
   	.catch((data) => {
   		debugger
   	})
-
-  	console.log(this.state.search_results)
   }
 
   handleSelect(e){
   	e.preventDefault()
-  	debugger
+  	const id = e.target.getAttribute('data-id')
+  	const activeCurrency = this.state.search_results.filter( item => item.id == parseInt(id))
+  	this.setState({
+  	  active_currency: activeCurrency[0],
+  	  search_results: []	
+  	})
   }
 
   render(){
+  	const searchOrCalculate = this.state.active_currency ? 
+  	<Calculate/> :
+  	<Search 
+  	  handleSelect={this.handleSelect} 
+  	  searchResults={this.state.search_results} 
+  	  handleChange={this.handleChange} />
+
   	return(
   	  <div>
-        <Search handleSelect={this.handleSelect} searchResults={this.state.search_results} handleChange={this.handleChange}/>
-        <Calculate/>
+        {searchOrCalculate}
   	  </div>
   	)
   }
