418658ffd7308e95b6181df864a3dd954467f953
transfer76 <shina76@ukr.net>
2019-08-14 21:56:32 +0300

    Add to components SearchResult

--- app/javascript/components/Search.js
+++ app/javascript/components/Search.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import SearchResult from './SearchResult'
 
 class Search extends Component {
   constructor(props){
@@ -6,15 +7,17 @@ class Search extends Component {
   }
 
   render(){
-  	const searchResults = this.props.searchResults.map( curr => <li key={curr.id} data-id={curr.id} onClick={this.props.handleSelect} className="currency-list-item"><a href="#" className="currency"><span>{curr.name}</span> <span>{curr.currency_symbol}</span></a></li>)
-  	
+  	const searchResults = this.props.searchResults.map( curr => 
+  	  <SearchResult key={curr.id} currency={curr}
+  	   handleSelect= {this.props.handleSelect} />)
   	return(
   	  <div>
         <h1>Cryptocurrency Portfolio Calculator</h1>
         <form>
           <div className="form-group">
             <label>Search for a Currency:</label><br/>
-            <input onChange={this.props.handleChange} autoComplete="off" type="text" name="name" placeholder="Ex: Bitcoin, Litecoin, Ethereum..." value={this.props.name} className="field"/>
+            <input onChange={this.props.handleChange} autoComplete="off" type="text" name="name" 
+             placeholder="Ex: Bitcoin, Litecoin, Ethereum..." value={this.props.name} className="field" />
           </div>
           <div className="currency-list">
             {searchResults}
